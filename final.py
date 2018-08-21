ec40aeebf6e6cc7f1b7d33aec333e471bdab3916
deepisha <dpsa.eivaa@gmail.com>
2018-08-21 20:39:02 +0545

    Add files via upload

---
+++ final.py
@@ -0,0 +1,151 @@
+import os
+import argparse
+import functions
+# import numpy as nps
+# from keras.layers import Dense, Activation, Dropout, Flatten
+# from keras.engine import Input
+from scipy.optimize import fmin_l_bfgs_b
+import time
+from keras import backend as K
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+parser = argparse.ArgumentParser(description='Neural style transfer with Keras.')
+parser.add_argument('content_image_path', metavar='content', type=str,
+                    help='Path to the image to transform.')
+parser.add_argument('style_reference_image_path', metavar='ref', type=str,
+                    help='Path to the style reference image.')
+parser.add_argument('result_prefix', metavar='res_prefix', type=str,
+                    help='Prefix for the saved results.')
+parser.add_argument('--iter', type=int, default=10, required=False,
+                    help='Number of iterations to run.')
+parser.add_argument('--content_weight', type=float, default=0.025, required=False,
+                    help='Content weight.')
+parser.add_argument('--style_weight', type=float, default=1.0, required=False,
+                    help='Style weight.')
+parser.add_argument('--tv_weight', type=float, default=1.0, required=False,
+                    help='Total Variation weight.')         
+args = parser.parse_args()
+content_image_path = args.content_image_path
+style_reference_image_path = args.style_reference_image_path
+result_prefix = args.result_prefix
+iterations = args.iter
+
+# these are the weights of the different loss components
+total_variation_weight = args.tv_weight
+style_weight = args.style_weight
+content_weight = args.content_weight
+
+# dimensions of the generated picture.
+width, height = functions.load_image(content_image_path).size
+
+# get tensor representations of our images
+content_image = K.variable(functions.preprocess_image(content_image_path, width, height))
+style_reference_image = K.variable(functions.preprocess_image(style_reference_image_path,width, height))
+
+# this will contain our generated image
+
+img_nrows, img_ncols = functions.calc_rowsandcols(width,height)
+if K.image_data_format() == 'channels_first':
+    combination_image = K.placeholder((1, 3, img_nrows, img_ncols))
+else:
+    combination_image = K.placeholder((1, img_nrows, img_ncols, 3))
+
+# combine the 3 images into a single Keras tensor
+input_tensor = K.concatenate([content_image,
+                              style_reference_image,
+                              combination_image], axis=0)
+
+# build the VGG16 network with our 3 images as input
+# the model will be loaded with pre-trained ImageNet weights
+model = functions.customVGG16(input_tensor)
+#print('Model loaded.')
+
+# get the symbolic outputs of each "key" layer (we gave them unique names).
+outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
+
+# compute the neural style loss
+# first we need to define 4 util functions
+
+
+# an auxiliary loss function
+# designed to maintain the "content" of the
+# content image in the generated image
+
+
+
+# combine these loss functions into a single scalar
+loss = K.variable(0.)
+layer_features = outputs_dict['conv2d_12']
+content_image_features = layer_features[0, :, :, :]
+combination_features = layer_features[2, :, :, :]
+loss += content_weight * functions.content_loss(content_image_features,
+                                      combination_features)
+
+feature_layers = ['conv2d_1', 'conv2d_3',
+                  'conv2d_5', 'conv2d_8',
+                  'conv2d_11']
+for layer_name in feature_layers:
+    layer_features = outputs_dict[layer_name]
+    style_reference_features = layer_features[1, :, :, :]
+    combination_features = layer_features[2, :, :, :]
+    sl = functions.style_loss(style_reference_features, combination_features,width,height)
+    loss += (style_weight / len(feature_layers)) * sl
+loss += total_variation_weight * functions.total_variation_loss(combination_image,width,height)
+
+# get the gradients of the generated image wrt the loss
+grads = K.gradients(loss, combination_image)
+
+outputs = [loss]
+if isinstance(grads, (list, tuple)):
+    outputs += grads
+else:
+    outputs.append(grads)
+
+f_outputs = K.function([combination_image], outputs)
+
+class Evaluator(object):
+
+    def __init__(self):
+        self.loss_value = None
+        self.grads_values = None
+
+    def loss(self, x):
+        assert self.loss_value is None
+        loss_value, grad_values = functions.eval_loss_and_grads(x, width, height, f_outputs)
+        self.loss_value = loss_value
+        self.grad_values = grad_values
+        return self.loss_value
+
+    def grads(self, x):
+        assert self.loss_value is not None
+        grad_values = np.copy(self.grad_values)
+        self.loss_value = None
+        self.grad_values = None
+        return grad_values
+
+evaluator = Evaluator()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+x = functions.preprocess_image(content_image_path,width,height)
+
+for i in range(iterations):
+    print('Start of iteration', i)
+    start_time = time.time()
+    # x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
+    #                                  fprime=evaluator.grads, maxfun=20)
+    
+    print('Current loss value:', min_val)
+    # save current generated image
+    img = deprocess_image(x.copy())
+    fname = result_prefix + '_at_iteration_%d.png' % i
+    Image.fromarray(img).save(fname)
+    # image.save_img(fname, img)
+    end_time = time.time()
+    print('Image saved as', fname)
+    print('Iteration %d completed in %ds' % (i, end_time - start_time))
---
+++ functions.py
@@ -0,0 +1,243 @@
+from keras.models import Sequential
+from keras.layers import MaxPooling2D, Conv2D, Input
+from PIL import Image as pil_image
+from keras import backend as K
+import numpy as np
+from keras.applications import vgg19
+
+def customVGG16(input_tensor):
+	vgg16 = vgg19.VGG19(weights='imagenet', include_top=False input_tensor = 'input_tensor')
+	input_shape = (224, 224, 3)
+	if not K.is_keras_tensor(input_tensor):
+		img_input = Input(tensor=input_tensor, shape=input_shape)
+	else:
+		img_input = input_tensor
+	model = Sequential()
+	model.add(Conv2D(64, (3, 3), dtype = 'float32', input_shape=input_shape, padding='same',
+           activation='relu', weights=vgg16.layers[1].get_weights()))#--> layer 1
+	model.add(Conv2D(64, (3, 3), activation='relu', padding='same', weights=vgg16.layers[2].get_weights()))#--> layer 2
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), weights=vgg16.layers[3].get_weights()))#--> layer 3
+	model.add(Conv2D(128, (3, 3), activation='relu', padding='same', weights=vgg16.layers[4].get_weights()))#--> layer 4
+	model.add(Conv2D(128, (3, 3), activation='relu', padding='same', weights=vgg16.layers[5].get_weights()))#--> layer 5
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), weights=vgg16.layers[6].get_weights()))#--> layer 6
+	model.add(Conv2D(256, (3, 3), activation='relu', padding='same', weights=vgg16.layers[7].get_weights()))#--> layer 7
+	model.add(Conv2D(256, (3, 3), activation='relu', padding='same', weights=vgg16.layers[8].get_weights())) #--> layer 8. 
+	model.add(Conv2D(256, (3, 3), activation='relu', padding='same', weights=vgg16.layers[9].get_weights()))#--> layer 9
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), weights=vgg16.layers[10].get_weights()))#--> layer 10
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same', weights=vgg16.layers[11].get_weights()))#--> layer 11
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same', weights=vgg16.layers[12].get_weights()))#--> layer 12
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same', weights=vgg16.layers[13].get_weights()))#--> layer 13
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), weights=vgg16.layers[14].get_weights()))#--> layer 14
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same',  weights=vgg16.layers[15].get_weights()))#--> layer 15
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same',  weights=vgg16.layers[16].get_weights()))#--> layer 16
+	model.add(Conv2D(512, (3, 3), activation='relu', padding='same',  weights=vgg16.layers[17].get_weights()))#--> layer 17
+	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), weights=vgg16.layers[18].get_weights()))#--> layer 18
+	return model
+
+def load_image(x, target_size=None):
+    img = pil_image.open(x)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    if target_size is not None:
+    	width_height_tuple = (target_size[1], target_size[0])
+    	img = img.resize(width_height_tuple, pil_image.NEAREST)
+    return img
+
+def img_to_array(img, data_format=None):
+    """Converts a PIL Image instance to a Numpy array.
+    # Arguments
+        img: PIL Image instance.
+        data_format: Image data format,
+            either "channels_first" or "channels_last".
+    # Returns
+        A 3D Numpy array.
+    # Raises
+        ValueError: if invalid `img` or `data_format` is passed.
+    """
+    
+    data_format = tf.image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format: ', data_format)
+    # Numpy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but original PIL image has format (width, height, channel)
+    x = np.asarray(img, dtype=backend.floatx())
+    if len(x.shape) == 3:
+        if data_format == 'channels_first':
+            x = x.transpose(2, 0, 1)
+    elif len(x.shape) == 2:
+        if data_format == 'channels_first':
+            x = x.reshape((1, x.shape[0], x.shape[1]))
+        else:
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+    else:
+        raise ValueError('Unsupported image shape: ', x.shape)
+    return x
+
+def img_to_array(img, data_format=None):
+    """Converts a PIL Image instance to a Numpy array.
+    # Arguments
+        img: PIL Image instance.
+        data_format: Image data format,
+            either "channels_first" or "channels_last".
+    # Returns
+        A 3D Numpy array.
+    # Raises
+        ValueError: if invalid `img` or `data_format` is passed.
+    """
+    data_format = K.image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format: ', data_format)
+    # Numpy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but original PIL image has format (width, height, channel)
+    x = np.asarray(img, dtype=K.floatx())
+    if len(x.shape) == 3:
+        if data_format == 'channels_first':
+            x = x.transpose(2, 0, 1)
+    elif len(x.shape) == 2:
+        if data_format == 'channels_first':
+            x = x.reshape((1, x.shape[0], x.shape[1]))
+        else:
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+    else:
+        raise ValueError('Unsupported image shape: ', x.shape)
+    return x    
+
+def calc_rowsandcols(width, height):
+	img_nrows = 400
+	img_ncols = int(width * img_nrows / height)
+	return img_nrows,img_ncols
+
+def preprocess_image(image_path, width, height):
+    # dimensions of the generated picture.
+    img_nrows = 400
+    img_ncols = int(width * img_nrows / height)
+    img = load_image(image_path, target_size=(img_nrows, img_ncols))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+def preprocess_input(x):
+	data_format = K.image_data_format()
+	x = x.astype(K.floatx(), copy=False)
+	if data_format == 'channels_first':
+		# 'RGB'->'BGR'
+		if x.ndim == 3:
+			x = x[::-1, ...]
+		else:
+			x = x[:, ::-1, ...]
+	else:
+		# 'RGB'->'BGR'
+		x = x[..., ::-1]
+	mean = [103.939, 116.779, 123.68]
+	std = None
+	if data_format == 'channels_first':
+		if x.ndim == 3:
+			x[0, :, :] -= mean[0]
+			x[1, :, :] -= mean[1]
+			x[2, :, :] -= mean[2]
+			if std is not None:
+				x[0, :, :] /= std[0]
+				x[1, :, :] /= std[1]
+				x[2, :, :] /= std[2]
+		else:
+			x[:, 0, :, :] -= mean[0]
+			x[:, 1, :, :] -= mean[1]
+			x[:, 2, :, :] -= mean[2]
+			if std is not None:
+				x[:, 0, :, :] /= std[0]
+				x[:, 1, :, :] /= std[1]
+				x[:, 2, :, :] /= std[2]
+	else:
+		x[..., 0] -= mean[0]
+		x[..., 1] -= mean[1]
+		x[..., 2] -= mean[2]
+		if std is not None:
+			x[..., 0] /= std[0]
+			x[..., 1] /= std[1]
+			x[..., 2] /= std[2]
+	return x
+
+def deprocess_image(x):
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((3, img_nrows, img_ncols))
+        x = x.transpose((1, 2, 0))
+    else:
+        x = x.reshape((img_nrows, img_ncols, 3))
+    # Remove zero-center by mean pixel
+    #add to respectice depths i.e 0,1,2
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    # 'BGR'->'RGB'
+    x = x[:, :, ::-1]
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+# the gram matrix of an image tensor (feature-wise outer product)
+
+
+def gram_matrix(x):
+    assert K.ndim(x) == 3
+    if K.image_data_format() == 'channels_first':
+        features = K.batch_flatten(x)
+    else:
+        features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+    return gram
+
+# the "style loss" is designed to maintain
+# the style of the reference image in the generated image.
+# It is contentd on the gram matrices (which capture style) of
+# feature maps from the style reference image
+# and from the generated image
+
+
+def style_loss(style, combination, width, height):
+    assert K.ndim(style) == 3
+    assert K.ndim(combination) == 3
+    S = gram_matrix(style)
+    C = gram_matrix(combination)
+    channels = 3
+    img_nrows = 400
+    img_ncols = int(width * img_nrows / height)
+    size = img_nrows * img_ncols
+    return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+
+def content_loss(content, combination):
+    return K.sum(K.square(combination - content))
+
+# the 3rd loss function, total variation loss,
+# designed to keep the generated image locally coherent
+
+
+def total_variation_loss(x,width,height):
+    img_nrows = 400
+    img_ncols = int(width * img_nrows / height)
+    assert K.ndim(x) == 4
+    if K.image_data_format() == 'channels_first':
+        a = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, 1:, :img_ncols - 1])
+        b = K.square(x[:, :, :img_nrows - 1, :img_ncols - 1] - x[:, :, :img_nrows - 1, 1:])
+    else:
+        a = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, 1:, :img_ncols - 1, :])
+        b = K.square(x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
+    return K.sum(K.pow(a + b, 1.25))
+
+def eval_loss_and_grads(x, width, height, f_outputs):
+    img_nrows = 400
+    img_ncols = int(width * img_nrows / height)
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((1, 3, img_nrows, img_ncols))
+    else:
+        x = x.reshape((1, img_nrows, img_ncols, 3))
+    outs = f_outputs([x])
+    loss_value = outs[0]
+    if len(outs[1:]) == 1:
+        grad_values = outs[1].flatten().astype('float64')
+    else:
+        grad_values = np.array(outs[1:]).flatten().astype('float64')
+    return loss_value, grad_values
+
+
